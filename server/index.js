1b0f536c1575f987e5bc2e764afc145d318340e9
devank <adreshsingh275127@gmail.com>
2026-02-19 22:28:36 +0530

    entery point for server

---
+++ server/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+
+import userRouter from './routes/auth.js';
+
+const app = express();
+dotenv.config(); // Load environment variables from .env file
+
+const PORT = process.env.PORT || 5000;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+
+
+// Middleware
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors())
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+// set up routes
+app.use("auth/", userRouter);
+
+mongoose.connect(MONGODB_URI).then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    }
+    );
+}).catch((error) => {
+    console.error("Error connecting to MongoDB:", error.message);
+});
